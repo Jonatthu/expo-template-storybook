d9ede4cd9a7fb3b9757e838ddeeb08027d3028c1
Jonathan Nungaray <jonatthu@gmail.com>
2022-11-20 15:38:10 -0800

    New changes

--- App.tsx
+++ App.tsx
@@ -18,28 +18,19 @@ const DecimalsButtons = [
 export default function App() {
     return (
         <View className='pt-28 flex-1'>
-            <Text className='text-base'>Hello</Text>
-            <Text className='text-lg'>Hello</Text>
-            <Text className='text-xl'>Hello</Text>
-            {/* This class 9xl is broken for example */}
-            <Text className='text-8xl'>Hello</Text>
-            <Text className='text-9xl'>Hello</Text>
-
-            <View className="p-2">
-                <View className="mb-4 flex-row">
-                    {DecimalsButtons.map((row, index) => (
-                        <View className="flex-1 bg-slate-300 gap-2" key={index}>
-                            {row.map((button) => (
-                                <Button
-                                    key={button}
-                                    className="h-16 items-center justify-center bg-green-100"
-                                >
-                                    <Text>{button}</Text>
-                                </Button>
-                            ))}
-                        </View>
-                    ))}
-                </View>
+            <View className="mb-4 flex-row">
+                {DecimalsButtons.map((row, index) => (
+                    <View className="flex-1 gap-2" key={index}>
+                        {row.map((button) => (
+                            <Button
+                                key={button}
+                                className="h-16 items-center justify-center bg-green-100"
+                            >
+                                <Text>{button}</Text>
+                            </Button>
+                        ))}
+                    </View>
+                ))}
             </View>
         </View>
     );
