82c0bf6506a3990d6a4f82b830b6d15834e935cd
Jonathan Nungaray <jonatthu@gmail.com>
2022-11-20 14:37:59 -0800

    New changes

--- App.tsx
+++ App.tsx
@@ -1,2 +1,40 @@
-import StorybookUIRoot from "./.ondevice/Storybook";
-export { StorybookUIRoot as default };
+import { TouchableOpacity as RNButton, Text as RNText, View as RNView } from 'react-native';
+
+import React from 'react';
+import { styled } from 'nativewind';
+
+const View = styled(RNView);
+const Button = styled(RNButton);
+const Text = styled(RNText);
+
+
+const DecimalsButtons = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+    ['.', '0', 'Del'],
+];
+
+export default function App() {
+    return (
+        <View className='pt-28 bg-purple-300 flex-1'>
+            <Text className='text-base'>Hello</Text>
+            <Text className='text-lg'>Hello</Text>
+            <Text className='text-xl'>Hello</Text>
+            <Text className='text-9xl'>Hello</Text>
+            <View className="bg-green-100 mb-4 gap-2">
+                {DecimalsButtons.map((row, index) => (
+                    <View className="flex-1 flex-row gap-2" key={index}>
+                        {row.map((button) => (
+                            <Button key={button} className="h-16 flex-1">
+                                <Text>{button}</Text>
+                            </Button>
+                        ))}
+                    </View>
+                ))}
+            </View>
+        </View>
+    );
+
+
+}
